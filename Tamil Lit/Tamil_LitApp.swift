d44d5fb64ad1527a2d911e045fed2b10188abb68
Selvarajan Thangavel <selvarajan2006@gmail.com>
2024-01-28 15:23:42 +0530

    Thirukural views impl

--- Tamil Lit/Tamil_LitApp.swift
+++ Tamil Lit/Tamil_LitApp.swift
@@ -10,23 +10,41 @@ import SwiftData
 
 @main
 struct Tamil_LitApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
+//    var sharedModelContainer: ModelContainer = {
+//        let schema = Schema([
+//            Item.self,
+//        ])
+//        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+//
+//        do {
+//            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+//        } catch {
+//            fatalError("Could not create ModelContainer: \(error)")
+//        }
+//    }()
+//
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//        }
+//        .modelContainer(sharedModelContainer)
+//    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                List {
+                    NavigationLink(destination: HomeView()) {
+                        Text("Thirukural")
+                    }
+                    
+                    NavigationLink(destination: HomeView()) {
+                        Text("Aathisudi")
+                    }
+                }
+                .navigationBarTitle("Tamil Lit App")
+                .navigationBarTitleDisplayMode(.inline)
+            }.preferredColorScheme(.light)
         }
-        .modelContainer(sharedModelContainer)
     }
 }
--- Tamil Lit/Thirukural/HomeView.swift
+++ Tamil Lit/Thirukural/HomeView.swift
@@ -9,7 +9,22 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        // Assuming jsonData is your JSON data
+        let decoder = JSONDecoder()
+        let decodedData = try! decoder.decode(KuralData.self, from: loadKuralData())
+        KuralListView(kuralData: decodedData.kural)
+    }
+    
+    func loadKuralData() -> Data {
+        guard let url = Bundle.main.url(forResource: "thirukkural", withExtension: "json") else {
+            fatalError("Failed to locate file in bundle.")
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load file from bundle.")
+        }
+        
+        return data
     }
 }
 

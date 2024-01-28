efe4be3eb1f58154ac1afcd5a3ae37dfa97202c6
Selvarajan Thangavel <selvarajan2006@gmail.com>
2024-01-28 15:22:12 +0530

    Adding Thirukural json data and building ui views for the verses

---
+++ Tamil Lit/Thirukural/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Tamil Lit
+//
+//  Created by Selvarajan on 28/01/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ Tamil Lit/Thirukural/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  Tamil Lit
+//
+//  Created by Selvarajan on 28/01/24.
+//
+
+import Foundation

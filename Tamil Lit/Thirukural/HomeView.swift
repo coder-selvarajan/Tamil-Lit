e12f487e0d648430ed554ad469846d5c775d0b8c
Selvarajan Thangavel <selvarajan2006@gmail.com>
2024-01-28 19:18:06 +0530

    Implementing a viewmodel for the operations

--- Tamil Lit/Thirukural/HomeView.swift
+++ Tamil Lit/Thirukural/HomeView.swift
@@ -8,26 +8,13 @@
 import SwiftUI
 
 struct HomeView: View {
-    var body: some View {
-        // Assuming jsonData is your JSON data
-        let decoder = JSONDecoder()
-        let decodedData = try! decoder.decode(KuralData.self, from: loadKuralData())
-        KuralListView(kuralData: decodedData.kural)
-    }
+    var vm: ThirukuralViewModel = ThirukuralViewModel()
     
-    func loadKuralData() -> Data {
-        guard let url = Bundle.main.url(forResource: "thirukkural", withExtension: "json") else {
-            fatalError("Failed to locate file in bundle.")
-        }
-        
-        guard let data = try? Data(contentsOf: url) else {
-            fatalError("Failed to load file from bundle.")
-        }
-        
-        return data
+    var body: some View {
+        KuralListView(kuralData: vm.loadKuralData())
     }
 }
 
-#Preview {
-    HomeView()
-}
+//#Preview {
+//    HomeView()
+//}

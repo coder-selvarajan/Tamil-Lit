ae80cc6790f7811c778dbabba33123ed0946d14e
Selvarajan Thangavel <selvarajan2006@gmail.com>
2024-01-28 13:28:01 +0530

    Initial commit

---
+++ Tamil Lit/VerseInfo.swift
@@ -0,0 +1,18 @@
+//
+//  VerseInfo.swift
+//  Tamil Lit
+//
+//  Created by Selvarajan on 28/01/24.
+//
+
+import SwiftUI
+
+struct VerseInfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VerseInfo()
+}

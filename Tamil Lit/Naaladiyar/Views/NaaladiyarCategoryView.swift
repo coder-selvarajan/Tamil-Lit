2b80ccc2539f949c6a7c59cf5cc200aa33b5a84b
Selvarajan Thangavel <selvarajan2006@gmail.com>
2024-03-24 23:06:25 +0530

    Adding views and models for Naaladiyar poems

---
+++ Tamil Lit/Naaladiyar/Views/NaaladiyarCategoryView.swift
@@ -0,0 +1,18 @@
+//
+//  NaaladiyarCategoryView.swift
+//  Tamil Lit
+//
+//  Created by Selvarajan on 24/03/24.
+//
+
+import SwiftUI
+
+struct NaaladiyarCategoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NaaladiyarCategoryView()
+}
---
+++ Tamil Lit/Naaladiyar/Views/NaaladiyarHomeView.swift
@@ -0,0 +1,18 @@
+//
+//  NaaladiyarHomeView.swift
+//  Tamil Lit
+//
+//  Created by Selvarajan on 24/03/24.
+//
+
+import SwiftUI
+
+struct NaaladiyarHomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NaaladiyarHomeView()
+}

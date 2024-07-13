f630c9b135f9157aa8c5094ac5059b5cc02c3b7e
Selvarajan Thangavel <selvarajan2006@gmail.com>
2024-07-13 13:09:36 +0530

    Cleaned up individual poem views - ui improvements

--- Tamil Lit/GenericUI/BookHomeView.swift
+++ Tamil Lit/GenericUI/BookHomeView.swift
@@ -16,18 +16,39 @@ struct BookHomeView: View {
     
     var body: some View {
         ZStack {
-            colorTheme.opacity(0.25).ignoresSafeArea()
+            colorTheme.opacity(0.3).ignoresSafeArea()
+            
             if viewModel.book?.categoryLevel == 1 {
                 PoemListWithCategoryView(colorTheme: colorTheme, bookName: bookName)
-                
-//                if bookName == "ஆத்திச்சூடி" {
-//                    SingleCategoryView(colorTheme: colorTheme, bookName: bookName)
-//                } else {
-//                    PoemListWithCategoryView(colorTheme: colorTheme, bookName: bookName)
-//                }
             } else {
                 CategoryListView(colorTheme: colorTheme, bookName: bookName)
             }
+            
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    
+                    Button {
+                        // Go to home page
+//                        print(navigationPath.count)
+                        //                            navigationPath.removeAll()
+                        //                            presentationMode.wrappedValue.dismiss()
+                    } label: {
+                        Image(systemName: "house.fill")
+                            .font(.title3)
+                            .foregroundStyle(.black.opacity(0.8))
+                            .padding(15)
+                    }
+                    .background(.white)
+                    .cornerRadius(10.0)
+                    .shadow(radius: 10)
+                    .padding(20)
+                    
+                }
+            }
+            .edgesIgnoringSafeArea(.bottom)
+            
         }.onAppear {
             viewModel.fetchAllData(bookname: bookName)
         }
